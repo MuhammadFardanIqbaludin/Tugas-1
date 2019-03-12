9dd01f45105ba19373e91c5445afc752cb0a54b6
MuhammadFardanIqbaludin <48473106+MuhammadFardanIqbaludin@users.noreply.github.com>
2019-03-12 15:58:43 +0700

    Add files via upload

---
+++ qw.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo " <h2>Hello Word!"</h2""
+echo " Nama : Muhammad Fardan Iqbaludin<br>";
+echo " Nim  : 16.01.53.0166<br>";
+
+?>
+<a href="https://www.unisbank.ac.id/v2/">Go To Akademik</a>
+
+<html>
+<body>
+
+
